affa1d89b9168fa93f3d7129b4fb98b79f8acc14
Benjamin Prost <benjamin.prost@periscope.digital>
2021-03-10 17:42:31 +0100

    darken background for accessibility

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-indigo-500 text-center py-2 mt-10 text-white text-sm">
+    <div class="bg-indigo-600 text-center py-2 mt-10 text-white text-sm">
         Made with ❤️ with <a class="text-white dark:text-white" href="https://fr.nuxtjs.org/" target="blank">Nuxt.js</a> 
     </div>
 </template>
